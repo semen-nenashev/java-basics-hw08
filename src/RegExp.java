6f3fe19c91ceb8b895fb43cc0fc4e7c1433e3609
semen-nenashev <nenashev.sd@gmail.com>
2021-01-29 11:43:12 +0200

    homework done

--- src/RegExp.java
+++ src/RegExp.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Regular expressions.
  */
@@ -11,6 +14,16 @@ public class RegExp {
      */
     public static String getIpAddress(String text) {
         // TODO fill in code here for extracting IP address using Regular Expression
-        return "";
+        String IPADDRESS_PATTERN =
+                "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+
+        Pattern pattern = Pattern.compile(IPADDRESS_PATTERN);
+        Matcher matcher = pattern.matcher(text);
+        String answer = new String();
+        if (matcher.find()) {
+            return answer = matcher.group();
+        } else {
+            return "";
+        }
     }
 }
